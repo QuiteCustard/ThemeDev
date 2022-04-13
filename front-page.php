62652759038558dc4e00492518edb167414ae9fe
SamEdwards141 <killercustard12@gmail.com>
2022-04-13 14:21:27 +0100

    Stylings
    Add fonts and new containers

--- front-page.php
+++ front-page.php
@@ -3,8 +3,6 @@ get_header();
 while( have_posts() ): ?>
     <?php the_post(); ?>
     <div class="actual-content <?php echo apply_filters( 'samsTheme_body_width_css', 'default' );?>">
-    <a class="" href="<?php the_permalink(); ?>">
-	</a>
         <?php the_content(); ?>
     </div>
 <?php endwhile; ?>
--- header.php
+++ header.php
@@ -6,6 +6,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="<?php echo get_bloginfo( 'description' ); ?>">
     <meta name="author" content="">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200;400;700&family=Roboto:wght@300;400&display=swap" rel="stylesheet">
 <?php wp_head();?>
 </head>
 <body class>
