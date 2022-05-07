cb074d46a76f716ed1cc4588e39b7f44c50fbf39
SamEdwards141 <killercustard12@gmail.com>
2022-05-07 15:56:44 +0100

    Template additons

--- templates/property_template.php
+++ templates/property_template.php
@@ -4,19 +4,23 @@ get_header();
 ?>
 <?php while( have_posts() ): ?>
     <?php the_post(); ?>
-    <div class="content-container">
+    <div class="content-container boxed">
         <h1 class="page-title"><?php the_title(); ?></h1>
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-8 actual-content">
-                    // Content here
-                </div>
-                <div class="col-sm-4">
-                    <div class="featured-image">
-                    </div>
-                </div>
+        <div class="actual-content">
+            <div class="grid">
+            <p>
+                <?php echo get_field('description'); ?>
+            </p>
+            <?php $image = get_field('property_image'); ?>
+            <img class="img" src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>">
+            <div class="cta">
+                <h3>Interested?</h3>
+                <p>If you'd like more information on <?php the_title();?>, get in touch here:</p>
+                <button class="button">Hi</button>
             </div>
+            <?php echo get_field('sleeps'); ?>
         </div>
     </div>
+</div>
 <?php endwhile; ?> 
 <?php get_footer(); ?>
